ddbdf4e80efa118dc9c48be7befe3b3dee6b53c6
ArtmMtvnko <artemm3note@gmail.com>
2025-05-09 20:40:15 +0300

    Added class and interface

---
+++ Kiosk/Modules/Accounting.fs
@@ -0,0 +1,55 @@
+﻿namespace Kiosk.Modules  
+
+module Accounting =  
+    open System
+
+    type IAccountable =  
+        abstract member Transactions: float list  
+        abstract member AddTransaction: float -> unit  
+        abstract member GetPureProfit: unit -> float  
+
+    type KioskAccount =  
+        { mutable Transactions: float list
+          Expenses: float }
+        interface IAccountable with  
+            member this.Transactions = this.Transactions  
+            member this.AddTransaction amount =  
+                this.Transactions <- amount :: this.Transactions  
+            member this.GetPureProfit() =
+                let profit = List.sum this.Transactions - this.Expenses
+                let taxes = 0.2
+                profit - (profit * taxes)
+
+    let extractStatistics (account: IAccountable) =
+        let numberOfTransactions = List.length account.Transactions
+        let profit = account.GetPureProfit ()
+        let average = List.average account.Transactions
+        let min = List.min account.Transactions
+        let max = List.max account.Transactions
+       
+        numberOfTransactions, profit, average, min, max
+
+    type Account(initial: float list) =
+        let mutable txns = initial
+        let mutable expenses = 0.0
+        let randomFactor = Random().NextDouble() * (0.7 - 0.2) + 0.2
+
+        do initial |> List.iter (fun txn -> expenses <- expenses + txn * randomFactor)
+
+        new () = Account([])
+
+        new (initial: float seq) = Account(Seq.toList initial)
+
+        member this.ProfitGross with get () = List.sum txns - expenses
+        member this.Expenses with get () = expenses
+
+        interface IAccountable with
+            member this.Transactions = txns
+            member this.AddTransaction amount =
+                txns <- amount :: txns
+                expenses <- expenses + (amount * randomFactor)
+                ()
+            member this.GetPureProfit() =
+                let profit = List.sum txns - expenses
+                let taxes = 0.2
+                profit * (1.0 - taxes)
--- Kiosk/Program.fs
+++ Kiosk/Program.fs
@@ -1,4 +1,7 @@
-﻿open Kiosk.Modules
+﻿module Program
+
+open Kiosk.Modules
+open Kiosk.Modules.Accounting
 open Kiosk.Types
 open Customer
 
@@ -23,7 +26,42 @@ let main args =
     let performCustomer1Replica = makeOrder customer1
     let performCustomer2Replica = makeOrder customer2
 
-    performCustomer1Replica "Hi! Can I get a burger? Also could you put some cheese and mushrooms?" |> ignore
-    performCustomer2Replica "Gimme that coffee man! And I wanna see some milk and sugar there" |> ignore
+    let income1 = performCustomer1Replica "Hi! Can I get a burger? Also could you put some cheese and mushrooms?"
+    let income2 = performCustomer2Replica "Gimme that coffee man! And I wanna see some milk and sugar there"
+
+    // Classic type declaration
+    let kioskAccount =
+        { Transactions = [income1; income2]
+          Expenses = 100.0 }
+
+    let (total, profit, avg, min, max) = extractStatistics kioskAccount
+
+    // Object expression
+    let mutable txns = [income1; income2]
+    let (total2, profit2, avg2, min2, max2) = 
+        extractStatistics
+            { new IAccountable with
+                member this.Transactions = txns
+                member this.AddTransaction amount =
+                    txns <- amount :: txns
+                member this.GetPureProfit () =
+                    let profit = List.sum txns - 30.0
+                    profit - (profit * 0.1)}
+
+    // Class declaration
+    let account = Account ([income1; income2])
+    let (total3, profit3, avg3, min3, max3) = extractStatistics account
+
+    let message: Printf.TextWriterFormat<_> = 
+            "Total transactions: %i \n
+             Profit: %f$ \n
+             Average transaction amount: %f$ \n
+             Mininum transaction amount: %f$ \n
+             Maximum transaction amount: %f$ \n"
+
+    printfn message total profit avg min max
+    printfn message total2 profit2 avg2 min2 max2
+    printfn message total3 profit3 avg3 min3 max3
+    printfn "Profit Gross: %f \n Total Expenses: %f \n" account.ProfitGross account.Expenses
 
     0
